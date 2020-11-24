23cc1bbc397219cb664abb3f6caad8f971ff1923
Aaron Dov Turkel <42723274+AaronDovTurkel@users.noreply.github.com>
2020-11-24 11:02:42 -0500

    automated user and password entry

--- 0b-preinstall-chroot.sh
+++ 0b-preinstall-chroot.sh
@@ -7,17 +7,22 @@
 #  Arch Linux Post Install Setup and Config
 #-----------------------------------------------------------------------
 
-read -p "Please enter hostname:" hostname
+read -sp "Please enter root password:" rootpassword
+read -sp "Please repeat root password:" rootpassword2
+
+# Check both passwords match
+if [ "$rootpassword" != "$rootpassword2" ]; then
+    echo "Passwords do not match, try running the script again"
+    exit 1
+fi
 
 read -p "Please enter username:" username
-
 read -sp "Please enter password:" password
-
 read -sp "Please repeat password:" password2
 
 # Check both passwords match
 if [ "$password" != "$password2" ]; then
-    echo "Passwords do not match"
+    echo "Passwords do not match, try running the script again"
     exit 1
 fi
 
@@ -49,25 +54,39 @@ for PKG in "${PKGS[@]}"; do
     pacman -S "$PKG" --noconfirm --needed
 done
 echo -e "\nDone!\n"
+
 systemctl enable sshd
 systemctl enable NetworkManager
+
 mkinitcpio -p linux
 mkinitcpio -p linux-lts
+
 sed -i "/en_US.UTF-8/s/^#//g" /etc/locale.gen
 locale-gen
-passwd
-useradd -m -g users -G wheel adt
-passwd adt
+
+# Setting root password
+echo $rootpassword | passwd –-stdin
+
+# creating user and setting password
+useradd -m -g users -G wheel $username
+echo $password | passwd –-stdin $username
+
 sed -i "/%wheel ALL=(ALL) ALL/s/^#//g" /etc/sudoers
+
+# Setting boot partition
 mkdir -p /boot/EFI
 mount /dev/nvme0n1p1 /boot/EFI
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
+
 mkdir /boot/grub/locale
 cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# Creating swapfile
 fallocate -l 2G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
+
 cp /etc/fstab /etc/fstab.bak
 echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
 cat /etc/fstab
